9704a9cbf9aedfe4b90bf7838121d816d9229884
Margarita Chepikova <margaritachepikova@yandex.ru>
2019-06-24 12:09:52 +0300

    Add solution

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,52 @@
-const solution = function(graph, start, finish)  {
-    // Ваше решение
-}
+const solution = function(graph, start, finish) {
+  const processed = [];
+  const costs = [];
+  const parents = {};
+
+  const findLowestCostNode = (costs) =>{
+    let lowestCost = Infinity;
+    let lowestCostNode = null;
+    for (let node in costs) {
+      let cost = costs[node];
+      if (cost < lowestCost && processed.indexOf(node) === -1) {
+        lowestCost = cost;
+        lowestCostNode = node;
+      }
+    }
+    return lowestCostNode;
+  };
+
+  Object.keys(graph).forEach(node => {
+    costs[node] = Infinity;
+  });
+
+  costs[start] = 0;
+
+  let node = findLowestCostNode(costs);
+  while (node) {
+    const cost = costs[node];
+    const neighbours = graph[node];
+    for (let n in neighbours) {
+      const newCost = cost + neighbours[n];
+      if (costs[n] > newCost) {
+        costs[n] = newCost;
+        parents[n] = node;
+      }
+    }
+    processed.push(node);
+    node = findLowestCostNode(costs);
+  }
+
+  const path = [finish];
+  let vertexParent = parents[finish];
+  while (vertexParent !== start) {
+    path.push(vertexParent);
+    vertexParent = parents[vertexParent];
+  }
+  path.push(vertexParent);
+
+  return {
+    distance: costs[finish],
+    path,
+  };
+};
